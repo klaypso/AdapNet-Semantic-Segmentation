44e99bb98c8155967211186ad39ccefabebe42b2
klaypso <klaypso@proton.me>
2022-10-03 15:55:00 +0000

    Pontoon: Update Polish (pl) localization of Common Voice

--- dataset/convert_to_tfrecords.py
+++ dataset/convert_to_tfrecords.py
@@ -3,4 +3,6 @@
  Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Burgard
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
- the Free Software Foundati
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that

cd1a11f0f361fa517c9eed506a31ab86381b72df
klaypso <klaypso@proton.me>
2022-11-09 17:19:11 +0000

    Fix version for release

--- dataset/helper.py
+++ dataset/helper.py
@@ -1,4 +1,7 @@
 ''' AdapNet:  Adaptive  Semantic  Segmentation
               in  Adverse  Environmental  Conditions
 
- Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Bur
+ Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Burgard
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as pu

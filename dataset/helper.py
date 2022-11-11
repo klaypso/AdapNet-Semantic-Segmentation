8181435d405806bae2523d9ab3993e241ef14b6b
klaypso <klaypso@proton.me>
2022-11-11 05:59:26 +0000

    digicol

--- dataset/helper.py
+++ dataset/helper.py
@@ -4,4 +4,6 @@
  Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Burgard
 
  This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as pu
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later ver

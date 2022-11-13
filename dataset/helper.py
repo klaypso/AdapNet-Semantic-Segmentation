30005037aa9002de774cc5392f3dc90b0d34eb72
klaypso <klaypso@proton.me>
2022-11-13 11:42:56 +0000

    Linter fixes

--- dataset/helper.py
+++ dataset/helper.py
@@ -9,4 +9,6 @@
  (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public Li

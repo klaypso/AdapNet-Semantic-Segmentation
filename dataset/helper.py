dd0cbc4a9ade5f8b361101fcda6afab3dca551f3
klaypso <klaypso@proton.me>
2022-11-16 18:37:16 +0000

    add details

--- dataset/helper.py
+++ dataset/helper.py
@@ -14,4 +14,9 @@
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with 
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+import numpy as np
+import tensorflow as tf
+
+def get_trai

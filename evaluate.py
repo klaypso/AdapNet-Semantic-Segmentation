19fd2b20f7c2b4b3baeef698fcdfba09b66204a5
klaypso <klaypso@proton.me>
2022-11-30 15:32:37 +0000

    Refactor Dockerfile

--- evaluate.py
+++ evaluate.py
@@ -3,4 +3,5 @@
 
  Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Burgard
 
- This progra
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as p

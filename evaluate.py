99dbec2ab5c96801f3a8ebea9e9987830cefd173
klaypso <klaypso@proton.me>
2022-11-30 15:32:24 +0000

    mtgjson update

--- evaluate.py
+++ evaluate.py
@@ -1,2 +1,6 @@
 ''' AdapNet:  Adaptive  Semantic  Segmentation
-              in  Adverse  Environmental 
+              in  Adverse  Environmental  Conditions
+
+ Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Burgard
+
+ This progra
